0701b966468a2a4bbdb72f1edf33fb2f6d6d22f7
Manisha0912 <19h61a04j8@cvsr.ac.in>
2025-03-16 14:19:10 +0530

    Fixed AppContext import issue

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,6 +1,33 @@
+// import {createContext} from "react";
+// import {beautician} from "../assets/assets"
+// // import Beautician from "../pages/Beautician";
+// // import AppContextProvider from "../Context/AppContext"; 
+
+
+
+// export const AppContext=createContext()
+// const AppContextProvider = (props) => {
+ 
+//     const value={
+//         beautician
+
+//     }
+//   return (
+//     <AppContext.Provider value={value}>
+//         {props.children}
+//         </AppContext.Provider>
+//   )
+// }
+
+// export default AppContextProvider
+
+
+
 import {createContext} from "react";
 import {beautician} from "../assets/assets"
-import Beautician from "../pages/Beautician";
+
+
+
 
 
 export const AppContext=createContext()
